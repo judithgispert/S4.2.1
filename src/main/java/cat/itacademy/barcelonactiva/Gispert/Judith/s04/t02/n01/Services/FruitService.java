e643f3df162c43046d734182b69d3d98878511ab
judithgispert <jugispert@gmail.com>
2024-01-17 10:23:22 +0100

    Create fruit service //TODO add info

---
+++ src/main/java/cat/itacademy/barcelonactiva/Gispert/Judith/s04/t02/n01/Services/FruitService.java
@@ -0,0 +1,9 @@
+package cat.itacademy.barcelonactiva.Gispert.Judith.s04.t02.n01.Services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class FruitService {
+
+    //TODO add repo + methods
+}
