57e9f93c25d56207f0f9c12612ea2ca4591eee27
judithgispert <jugispert@gmail.com>
2024-01-19 11:11:10 +0100

    Create interface + methods

---
+++ src/main/java/cat/itacademy/barcelonactiva/Gispert/Judith/s04/t02/n01/Services/IFruitService.java
@@ -0,0 +1,14 @@
+package cat.itacademy.barcelonactiva.Gispert.Judith.s04.t02.n01.Services;
+
+import cat.itacademy.barcelonactiva.Gispert.Judith.s04.t02.n01.Domain.Fruit;
+
+import java.util.List;
+
+public interface IFruitService {
+    public void createFruit(Fruit fruit);
+    public Fruit getFruitById(int id);
+    public List<Fruit> getFruits();
+    public Fruit updateFruit(Fruit fruit, int id);
+    public boolean deleteFruit(int id);
+
+}
